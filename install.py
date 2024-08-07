221854b74f233382724c2aa9fc25d22ea44eb6bc
chip <chip@codime.io>
2024-08-07 17:01:39 +0900

    Add install.py

---
+++ install.py
@@ -0,0 +1,31 @@
+import launch
+
+needs_install = True
+try:
+    import schp
+    needs_install = False
+except ImportError:
+    needs_install = True
+
+
+if needs_install:
+    import launch
+    import os
+    import pkg_resources
+
+    req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
+
+    with open(req_file) as file:
+        for package in file:
+            try:
+                package = package.strip()
+                if '==' in package:
+                    package_name, package_version = package.split('==')
+                    installed_version = pkg_resources.get_distribution(package_name).version
+                    if installed_version != package_version:
+                        launch.run_pip(f"install {package}", f"sd-webui-clothes-segmentation requirement: changing {package_name} version from {installed_version} to {package_version}")
+                elif not launch.is_installed(package):
+                    launch.run_pip(f"install {package}", f"sd-webui-clothes-segmentation requirement: {package}")
+            except Exception as e:
+                print(e)
+                print(f'Warning: Failed to install {package}, some preprocessors may not work.')
